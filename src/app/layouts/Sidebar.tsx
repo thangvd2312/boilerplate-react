a84ef5603fcf7f0b64217c07eb79d583f1ad170d
thangvd2312 <thangvd2312@gmail.com>
2022-12-21 14:33:59 +0700

    commit base

--- src/app/layouts/Sidebar.tsx
+++ src/app/layouts/Sidebar.tsx
@@ -1,41 +1,57 @@
-import { Layout, Menu, MenuProps } from 'antd';
-import React from 'react';
+import { DesktopOutlined, FileOutlined, PieChartOutlined, TeamOutlined, UserOutlined } from '@ant-design/icons';
+import { Breadcrumb, Layout, Menu, MenuProps } from 'antd';
+import React, { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+
+const { Header, Content, Footer, Sider } = Layout;
 
 type MenuItem = Required<MenuProps>['items'][number];
 
-const { Sider } = Layout;
-
-function getItem(label: React.ReactNode, key: React.Key, children?: MenuItem[], type?: 'group'): MenuItem {
+function getItem(label: React.ReactNode, key: React.Key, icon?: React.ReactNode, children?: MenuItem[]): MenuItem {
   return {
     key,
+    icon,
     children,
     label,
-    type,
   } as MenuItem;
 }
 
-const items: MenuItem[] = [];
+const items: MenuItem[] = [
+  getItem('Option 1', '1', <PieChartOutlined />),
+  getItem('Option 2', '2', <DesktopOutlined />),
+  getItem('User', 'sub1', <UserOutlined />, [getItem('Tom', '3'), getItem('Bill', '4'), getItem('Alex', '5')]),
+  getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+  getItem('Files', '9', <FileOutlined />),
+];
 
 function Sidebar() {
   const { pathname } = useLocation();
   const navigate = useNavigate();
+
+  const [collapsed, setCollapsed] = useState(false);
 
   function handleSelectItem(data: { key: string }) {
     navigate(data.key);
   }
 
   return (
-    <Sider theme="dark" breakpoint="lg" collapsedWidth="0" className="text-white">
-      <Menu
-        defaultSelectedKeys={[pathname]}
-        defaultOpenKeys={['notes', 'master']}
-        mode="inline"
-        theme="dark"
-        items={items}
-        onSelect={handleSelectItem}
-      />
-    </Sider>
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+        <div style={{ height: 32, margin: 16, background: 'rgba(255, 255, 255, 0.2)' }} />
+        <Menu theme="dark" mode="inline" items={items} defaultSelectedKeys={[pathname]} onSelect={handleSelectItem} />
+      </Sider>
+      <Layout className="site-layout">
+        <Header style={{ padding: 0, background: '#fff' }} />
+        <Content style={{ margin: '0 16px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div style={{ padding: 24, minHeight: 360, background: '#fff' }}>Bill is a cat.</div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+      </Layout>
+    </Layout>
   );
 }
 
